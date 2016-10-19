90b5ac176caf630ccfa35634b360a34ab36f1c6c
Unknown <yvonne.olij@outlook.com>
2016-10-19 13:19:47 +0200

    Added javascript

--- src/nl/java8cursus/rest/DataOphalen.java
+++ src/nl/java8cursus/rest/DataOphalen.java
@@ -10,41 +10,37 @@ import javax.ws.rs.core.MediaType;
 
 //import nl.luminis.javatraining.pojo.Person;
 
-@Path("/HelloWorld")
+@Path("/aanbod")
 public class DataOphalen
 {
 
+	
 /*	@GET
-	@Path("/sayHello")
-	public String sayHello()
-	{
-		return "<h1>Hello World</h1>";
-	}
-
-	@GET
 	@Path("/person/1")
 	@Produces(MediaType.APPLICATION_JSON)
 	public RestJobAanbod getPerson()
 	{
 		return new RestJobAanbod("Donald", "Duck");
 	} */
-
 	@GET
 	@Path("/person")
 	@Produces(MediaType.APPLICATION_JSON)
 	public List<RestJobAanbod> getPersons()
 	{
 		List<RestJobAanbod> personList = new ArrayList<>();
-		personList.add(new RestJobAanbod("Theo",  "Pont")); 
-		personList.add(new RestJobAanbod("Marco", "Wenneker"));
-		personList.add(new RestJobAanbod("Tai", "Chun Lee"));
-		personList.add(new RestJobAanbod("Geert", "Janssen"));
-		personList.add(new RestJobAanbod("Joey", "Krijgsman"));
-		personList.add(new RestJobAanbod("Erik", "Bouts"));
-		personList.add(new RestJobAanbod("Ronald", "van den Berg"));
-		personList.add(new RestJobAanbod("Yvonne", "Olij-Kenyon"));
-		personList.add(new RestJobAanbod("Edward", "Jong-A-Lock"));
-		personList.add(new RestJobAanbod("Peter", "Davidse"));
+		personList.add(new RestJobAanbod("Theo",  "Pont", "Images/theo.png", "theo skills"));
+		personList.add(new RestJobAanbod("Marco", "Wenneker","Images/marco.png", "theo skills"));
+		personList.add(new RestJobAanbod("Tai", "Chun Lee","Images/tai.png", "theo skills"));
+		personList.add(new RestJobAanbod("Geert", "Janssen","Images/geert.png", "theo skills"));
+		personList.add(new RestJobAanbod("Joey", "Krijgsman","Images/joey.png", "theo skills"));
+		personList.add(new RestJobAanbod("Erik", "Bouts","Images/erik.png", "theo skills"));
+		personList.add(new RestJobAanbod("Ronald", "van den Berg","Images/ronald.png", "theo skills"));
+		personList.add(new RestJobAanbod("Yvonne", "Olij-Kenyon","Images/yvonne.png", "theo skills"));
+		personList.add(new RestJobAanbod("Edward", "Jong-A-Lock","Images/edward.png", "theo skills"));
+		personList.add(new RestJobAanbod("Peter", "Davidse","Images/peter.png", "theo skills"));
+		
 		return personList;
+
 	}
+
 }
--- src/nl/java8cursus/rest/RestJobAanbod.java
+++ src/nl/java8cursus/rest/RestJobAanbod.java
@@ -7,10 +7,14 @@ public class RestJobAanbod implements Serializable
 	private static final long serialVersionUID = 1L;
 	private String firstName;
 	private String lastName;
+	private String plaatje;
+	private String skills;
 
-	public RestJobAanbod(String aFirstName, String aLastName) {
+	public RestJobAanbod(String aFirstName, String aLastName, String aPlaatje, String aSkills) {
 			setFirstName(aFirstName);
 			setLastName(aLastName);
+			setPlaatje(aPlaatje);
+			setSkills(aSkills);
 		}
 
 	public String getFirstName()
@@ -18,6 +22,11 @@ public class RestJobAanbod implements Serializable
 		return firstName;
 	}
 
+	public String getFirstName1(int indexRestJobAanbodObject)
+	{
+		return firstName;
+	}
+	
 	public void setFirstName(String firstName)
 	{
 		this.firstName = firstName;
@@ -31,5 +40,25 @@ public class RestJobAanbod implements Serializable
 	public void setLastName(String lastName)
 	{
 		this.lastName = lastName;
+	}
+
+	public String getPlaatje()
+	{
+		return plaatje;
+	}
+
+	public void setPlaatje(String plaatje)
+	{
+		this.plaatje = plaatje;
+	}
+
+	public String getSkills()
+	{
+		return skills;
+	}
+
+	public void setSkills(String skills)
+	{
+		this.skills = skills;
 	}
 }
